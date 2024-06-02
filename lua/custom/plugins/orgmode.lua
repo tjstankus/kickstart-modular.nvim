62fc25dcafd44924aea516372797732bb994dd16
TJ Stankus <tjstankus@gmail.com>
2024-06-02 11:45:29 -0400

    conceal org mode links

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -9,6 +9,9 @@ return {
       org_default_notes_file = '~/beorg/inbox.org',
     }
 
+    vim.opt.conceallevel = 2
+    vim.opt.concealcursor = 'nc'
+
     -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
     -- add ~org~ to ignore_install
     -- require('nvim-treesitter.configs').setup({
